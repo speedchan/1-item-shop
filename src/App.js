035cff661b880dd9c4003d90f58795d654374e88
Speed C <speedchanjun@gmail.com>
2021-01-20 23:29:34 +1000

    [src/App] Updated imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Checkout from './components/Checkout';
+import { React, useState } from 'react';
 
 function App() {
+
+  // dropdownValues is the state var name, updateDropdownValues is the func to update dropdownValues
+  const [dropdownValues, updateDropdownValues] = useState(
+    [
+      {
+        "amount": 6,
+        "cost": 12
+      },
+      {
+        "amount": 12,
+        "cost": 24
+      },
+      {
+        "amount": 24,
+        "cost": 48
+      }
+    ])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form dropdownValues={dropdownValues}/>
       </header>
     </div>
   );

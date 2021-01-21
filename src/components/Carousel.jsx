5eebd569e2b3f794b27e710e67df1f3309016449
Speed C <speedchanjun@gmail.com>
2021-01-21 16:21:12 +1000

    [components/multiple] Added new components (Carousel) and renamed existing components for clarity

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import logo from '../logo.svg';
+
+const Carousel = props => {
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
+    )
+}
+
+Carousel.propTypes = {
+
+}
+
+export default Carousel
---
+++ src/components/ItemSelectionForm.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import { itemPluralName } from '../Constants'
+
+
+const ItemSelectionForm = ({ itemAmount, onSelectItem }) => {
+    // Variables
+
+    // Methods
+    let onSelectChange = (e) => {
+        console.log(e.target.value);
+        onSelectItem(e.target.value)
+    };
+
+    // HTML
+    let dropdownOptions = itemAmount.map((item) => <option value={item} key={item}>{item}</option>);
+
+    return (
+        <form action="">
+            <label htmlFor=""></label>
+            <select onChange={onSelectChange}>
+                {dropdownOptions}
+            </select> {itemPluralName}
+        </form>
+    );
+}
+
+// Property types
+ItemSelectionForm.propTypes = {
+    itemAmount: PropTypes.array.isRequired
+}
+
+export default ItemSelectionForm;
